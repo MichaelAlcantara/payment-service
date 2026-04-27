1691b49e7913fbe3234b8cd6747ef473f55dea76
michael <michael.raposo@hotmail.com>
2026-04-27 18:19:14 -0300

    Implementando payment-topic KafkaAdmin

---
+++ payment-service/src/main/java/com/michaelAlcantara/payment_service/config/KafkaAdminConfig.java
@@ -0,0 +1,33 @@
+package com.michaelAlcantara.payment_service.config;
+
+import java.util.HashMap;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaAdminConfig {
+	
+	@Autowired
+	private KafkaProperties properties;
+	
+	@Bean
+	public KafkaAdmin kafkaAdmin() {
+		var config = new HashMap<String, Object>();
+		config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+		return new KafkaAdmin(config);
+	}
+	
+	@Bean
+	public KafkaAdmin.NewTopics newTopics(){
+		return new KafkaAdmin.NewTopics(
+			TopicBuilder.name("payment-topic").partitions(1).build()	
+		);
+	}
+
+}
