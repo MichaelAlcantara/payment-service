3d4285f095ea46028c2406b8959ef8b188266fb6
michael <michael.raposo@hotmail.com>
2026-04-27 20:50:13 -0300

    Configurando produtor para objetos Json

--- payment-service/src/main/java/com/michaelAlcantara/payment_service/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/michaelAlcantara/payment_service/service/impl/PaymentServiceImpl.java
@@ -1,20 +1,37 @@
 package com.michaelAlcantara.payment_service.service.impl;
 
+import java.io.Serializable;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.michaelAlcantara.payment_service.model.Payment;
 import com.michaelAlcantara.payment_service.service.PaymentService;
 
 @Service
-public class PaymentServiceImpl implements PaymentService{
-	
+public class PaymentServiceImpl implements PaymentService {
+
 	private static final Logger log = LogManager.getLogger(PaymentServiceImpl.class);
+
+	@Autowired
+	private KafkaTemplate<String, Serializable> kafkaTemplate;
 
 	@Override
 	public void sendPayment(Payment payment) {
 		log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+		try {
+
+			Thread.sleep(1000);
+
+			log.info("Enviando Pagamento ...");
+
+			kafkaTemplate.send("payment-topic", payment);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
