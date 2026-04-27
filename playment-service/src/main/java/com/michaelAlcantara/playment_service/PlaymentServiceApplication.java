3f601bda5f2140056619d8c6b25909301b088f29
michael <michael.raposo@hotmail.com>
2026-04-27 16:59:49 -0300

    Iniciando projeto

---
+++ playment-service/src/main/java/com/michaelAlcantara/playment_service/PlaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.michaelAlcantara.playment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaymentServiceApplication.class, args);
+	}
+
+}
---
+++ playment-service/src/test/java/com/michaelAlcantara/playment_service/PlaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelAlcantara.playment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
